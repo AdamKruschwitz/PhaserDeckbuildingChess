21eaca86671ebd6e8e8ee02dbb0327f8dd65ebf8
Adam Kruschwitz <akruschwitz@ithaca.edu>
2021-03-09 19:29:38 -0800

    Created and Colored Grid

--- js/main.js
+++ js/main.js
@@ -2,12 +2,7 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-        gravity: { y: 200 }
-      }
-  },
+  backgroundColor: '#c5c5c5',
   scene: {
     preload: preload,
     create: create
@@ -15,14 +10,19 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-game.config.backgroundColor.setFromHSV(0, 0, .75);
 
 function preload ()
 {
   this.load.svg('kingB', '/assets/KingB.svg');
+  this.load.svg('kingW', '/assets/KingW.svg');
 }
 
 function create ()
 {
-  this.add.image(400, 300, 'kingB');
+  var grid = this.add.grid(400, 300, 45*4, 45*4, 45, 45, 0xEEEEEE).setAltFillStyle(0x3F3F3F).setOutlineStyle();
+
+  this.add.image(400-22.5, 300-22.5, 'kingB');
+  this.add.image(400-22.5, 300+22.5, 'kingB');
+  this.add.image(400+22.5, 300+22.5, 'kingW');
+  this.add.image(400+22.5, 300-22.5, 'kingW');
 }
